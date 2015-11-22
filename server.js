ceb8f0fa0e41cd6a449a82763b48c92eaea02963
toeysk130 <toey.puakpipat@gmail.com>
2015-11-22 21:51:00 +0700

    Add line port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var static = require('node-static');
 var http = require('http');
+var port = process.env.PORT || 1337;
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(2013);
+}).listen(port);
