662d35f844b41c55e529408146ca6c60e4355071
toeysk130 <toey.puakpipat@gmail.com>
2015-11-22 22:09:39 +0700

    Edit port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var static = require('node-static');
 var http = require('http');
+var port = process.env.PORT || 1337;
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(2013);
+}).listen(port);
 
 var io = require('socket.io').listen(app);
 
